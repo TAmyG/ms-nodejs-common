5a9b4de0c1f0398739b67ff00368719d06faaa51
tamy <tamy.vivas@gmail.com>
2023-12-31 13:54:52 -0600

    ts initial config

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const color = {
+    red: 12,
+    blue: 23,
+    green: 34
+};
+console.log(color);
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const color: Color = {
+    red: 12,
+    blue: 23,
+    green: 34
+};
+
+
+console.log(color);
