dd4e98242f276cf1e75f8a176b7239ac8a07d3d2
tamy <tamy.vivas@gmail.com>
2023-12-31 14:26:10 -0600

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const color: Color = {
-    red: 1232,
-    blue: 23,
-    green: 34
-};
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
 
 
-console.log(color);
-
-export default color;
